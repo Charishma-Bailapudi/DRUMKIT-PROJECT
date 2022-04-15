e2dd1358679df42e9c245ba787bdcd9c2bacf17d
Charishma Bailapudi <98004429+Charishma-Bailapudi@users.noreply.github.com>
2022-04-15 21:49:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -119,5 +119,84 @@ document.addEventListener('keydown',function(e){
         }
     })
 
-    
+  icons[1].addEventListener('click',function(){
+        clap.play();
+        icons[1].style.boxShadow = "0px 0px 10px yellow";
+        icons[1].style.transform = "scale(1.1)";
+        setTimeout(function(){
+        icons[1].style.boxShadow = "0px 5px 20px gray";
+        icons[1].style.transform = "scale(1)";
+        },100);
+    })
+
+    icons[2].addEventListener('click',function(){
+        hithat.play();
+        icons[2].style.boxShadow = "0px 0px 10px yellow";
+        icons[2].style.transform = "scale(1.1)";
+        setTimeout(function(){
+        icons[2].style.boxShadow = "0px 5px 20px gray";
+        icons[2].style.transform = "scale(1)";
+        },100);
+    })
+
+    icons[3].addEventListener('click',function(){
+        kick.play();
+        icons[3].style.boxShadow = "0px 0px 10px yellow";
+        icons[3].style.transform = "scale(1.1)";
+        setTimeout(function(){
+        icons[3].style.boxShadow = "0px 5px 20px gray";
+        icons[3].style.transform = "scale(1)";
+        },100);
+    })
+
+    icons[4].addEventListener('click',function(){
+        openhat.play();
+        icons[4].style.boxShadow = "0px 0px 10px yellow";
+        icons[4].style.transform = "scale(1.1)";
+        setTimeout(function(){
+        icons[4].style.boxShadow = "0px 5px 20px gray";
+        icons[4].style.transform = "scale(1)";
+        },100);
+    })
+
+    icons[5].addEventListener('click',function(){
+        ride.play();
+        icons[5].style.boxShadow = "0px 0px 10px yellow";
+        icons[5].style.transform = "scale(1.1)";
+        setTimeout(function(){
+        icons[5].style.boxShadow = "0px 5px 20px gray";
+        icons[5].style.transform = "scale(1)";
+        },100);
+    })
+
+    icons[6].addEventListener('click',function(){
+        snare.play();
+        icons[6].style.boxShadow = "0px 0px 10px yellow";
+        icons[6].style.transform = "scale(1.1)";
+        setTimeout(function(){
+        icons[6].style.boxShadow = "0px 5px 20px gray";
+        icons[6].style.transform = "scale(1)";
+        },100);
+    })
+
+    icons[7].addEventListener('click',function(){
+        tink.play();
+        icons[7].style.boxShadow = "0px 0px 10px yellow";
+        icons[7].style.transform = "scale(1.1)";
+        setTimeout(function(){
+        icons[7].style.boxShadow = "0px 5px 20px gray";
+        icons[7].style.transform = "scale(1)";
+        },100);
+    })
+
+    icons[8].addEventListener('click',function(){
+        tom.play();
+        icons[8].style.boxShadow = "0px 0px 10px yellow";
+        icons[8].style.transform = "scale(1.1)";
+        setTimeout(function(){
+        icons[8].style.boxShadow = "0px 5px 20px gray";
+        icons[8].style.transform = "scale(1)";
+        },100);
+    })
+  
 
